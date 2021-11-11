b3ad657de823e23739a16235f1b3ab6ecb9539a2
C <earthhominid@gmail.com>
2021-11-11 14:13:13 -0300

    work experience & school experience css styling and components updated

--- src/components/School.js
+++ src/components/School.js
@@ -3,33 +3,45 @@ import React from 'react';
 function School() {
   return (
     <div>
-      <h2>Education</h2>
-      <form className="education_form">
-        <label className="form_text">Educational Institution:</label>
-        <input></input>
-        <label className="form_text">Program:</label>
-        <input></input>
-        <label className="form_text" for="start">
-          Start date:
-        </label>
-        <input
-          type="date"
-          id="start"
-          name="program-start"
-          min="1950-01-01"
-          max="2025-01-01"
-        ></input>
-        <label className="form_text" for="start">
-          End date:
-        </label>
-        <input
-          type="date"
-          id="end"
-          name="program-end"
-          min="1950-01-01"
-          max="2025-01-01"
-        ></input>
-      </form>
+      <div className="education_header">
+        <div className="edu_headline_circle">
+          <h2 className="education_title">Education</h2>
+        </div>
+      </div>
+      <div className="main_form_card">
+        <form className="education_form">
+          <label className="form_text"> Institution:</label>
+          <input className="education_input"></input>
+          <label className="form_text">Program:</label>
+          <input className="education_input"></input>
+          <label className="form_text" for="start">
+            Start date:
+          </label>
+          <input
+            className="education_input"
+            type="date"
+            id="start"
+            name="program-start"
+            min="1950-01-01"
+            max="2025-01-01"
+          ></input>
+          <label className="form_text" for="start">
+            End date:
+          </label>
+          <input
+            className="education_input"
+            type="date"
+            id="end"
+            name="program-end"
+            min="1950-01-01"
+            max="2025-01-01"
+          ></input>
+          <div className="button_holder">
+            <input id="edu_submit_button" type="submit" value="Submit"></input>
+            <input id="reset_button" type="reset"></input>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
