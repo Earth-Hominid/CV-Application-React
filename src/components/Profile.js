51355689e74ee7f35f77b3a1c35cfadcb5453c10
C <earthhominid@gmail.com>
2021-11-19 14:12:32 -0300

    changed profile function into class component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Profile() {
-  return (
-    <div>
-      <div className="card_header">
-        <h2 className="welcome_header">Professional Summary</h2>
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card_header">
+          <h2 className="welcome_header">Professional Summary</h2>
+        </div>
+        <div className="p_center">
+          <p>
+            Write a professional statement describing your relevant experience,
+            skills, and achievements. Tailor the statement to the companies
+            needs if possible.
+          </p>
+        </div>
+        <div className="main_form_card">
+          <form className="education_form">
+            <textarea
+              type="text"
+              id="professional_summary"
+              name="profile"
+              className="textarea_input"
+            ></textarea>
+            <div className="button_holder">
+              <input id="submit_button" type="submit" value="Submit"></input>
+              <input id="reset_button" type="reset"></input>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="p_center">
-        <p>
-          Write a professional statement describing your relevant experience,
-          skills, and achievements. Tailor the statement to the companies needs
-          if possible.
-        </p>
-      </div>
-      <div className="main_form_card">
-        <form className="education_form">
-          <textarea
-            type="text"
-            id="professional_summary"
-            name="profile"
-            className="textarea_input"
-          ></textarea>
-          <div className="button_holder">
-            <input id="submit_button" type="submit" value="Submit"></input>
-            <input id="reset_button" type="reset"></input>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Profile;
